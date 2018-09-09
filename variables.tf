d9e03dd1ee87944a0b55c5be8ae1c7034762767e
hdnebat <althunibat@outlook.com>
2018-09-09 10:22:19 +0400

    Initial Code

---
+++ variables.tf
@@ -0,0 +1,90 @@
+#====================#
+# vCenter connection #
+#====================#
+
+variable "vsphere_user" {
+  description = "vSphere user name"
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+}
+
+variable "vsphere_vcenter" {
+  description = "vCenter server FQDN or IP"
+}
+
+variable "vsphere_unverified_ssl" {
+  description = "Is the vCenter using a self signed certificate (true/false)"
+}
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+}
+
+variable "vsphere_drs_cluster" {
+  description = "vSphere cluster"
+  default     = ""
+}
+
+variable "vsphere_resource_pool" {
+  description = "vSphere resource pool"
+}
+
+
+variable "vm_user" {
+  description = "SSH user for the vSphere virtual machines"
+}
+
+variable "vm_password" {
+  description = "SSH password for the vSphere virtual machines"
+}
+
+variable "vm_datastore" {
+  description = "Datastore used for the vSphere virtual machines"
+}
+
+variable "vm_network" {
+  description = "Network used for the vSphere virtual machines"
+}
+
+variable "vm_template" {
+  description = "Template used to create the vSphere virtual machines"
+}
+
+variable "vm_folder" {
+  description = "vSphere Virtual machines folder"
+}
+
+variable "vm_linked_clone" {
+  description = "Use linked clone to create the vSphere virtual machines from the template (true/false). If you would like to use the linked clone feature, your template need to have one and only one snapshot"
+}
+
+variable "vm_ip" {
+  description = "IP used for VM"
+}
+
+variable "lab_netmask" {
+  description = "Netmask used for the Lab (example: 24)"
+}
+
+variable "lab_gateway" {
+  description = "Gateway for the lab nodes"
+}
+
+variable "lab_dns" {
+  description = "DNS for the lab nodes"
+}
+
+variable "lab_domain" {
+  description = "Domain for the lab nodes"
+}
+
+
+variable "vm_cpu" {
+  description = "Number of vCPU for the  virtual machine"
+}
+
+variable "vm_ram" {
+  description = "Amount of RAM for the virtual machine (example: 1024)"
+}
---
+++ vsphere-vm.tf
@@ -0,0 +1,111 @@
+#===============================================================================
+# vSphere Provider
+#===============================================================================
+
+provider "vsphere" {
+  version        = "1.7.0"
+  vsphere_server = "${var.vsphere_vcenter}"
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+
+  allow_unverified_ssl = "${var.vsphere_unverified_ssl}"
+}
+
+#===============================================================================
+# vSphere Data
+#===============================================================================
+
+data "vsphere_datacenter" "dc" {
+  name = "${var.vsphere_datacenter}"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "${var.vsphere_drs_cluster}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "${var.vsphere_resource_pool}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_datastore_cluster" "datastore_cluster" {
+  name          = "${var.vm_datastore}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "${var.vm_network}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "${var.vm_template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+#===============================================================================
+# vSphere Resources
+#===============================================================================
+
+# Create a virtual machine folder for the Kubernetes VMs #
+resource "vsphere_folder" "folder" {
+  path          = "${var.vm_folder}"
+  type          = "vm"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "testvm" {
+  name             = "lab-test-vm"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_cluster_id = "${data.vsphere_datastore_cluster.datastore_cluster.id}"
+  folder           = "${vsphere_folder.folder.path}"
+
+  num_cpus = "${var.vm_cpu}"
+  memory   = "${var.vm_ram}"
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "testvm.vmdk"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    linked_clone  = "${var.vm_linked_clone}"
+
+    customize {
+      linux_options {
+        host_name = "test-vm"
+        domain    = "${var.lab_domain}"
+      }
+
+      network_interface {
+        ipv4_address = "${var.vm_ip}"
+        ipv4_netmask = "${var.lab_netmask}"
+      }
+
+      ipv4_gateway    = "${var.lab_gateway}"
+      dns_server_list = ["${var.lab_dns}"]
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "${var.vm_user}"
+      password = "${var.vm_password}"
+    }
+
+    inline = [
+      "sudo apt-get install nginx",
+      "sudo systemctl start nginx",
+    ]
+  }
+}
