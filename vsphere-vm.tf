a2a2a6fd23d6b1f70a80f0c22f8bcaea4f065252
Hamza Althunibat <Hamza.Althunibat@moe.gov.ae>
2018-09-10 19:05:54 +0400

    fixes

--- vsphere-vm.tf
+++ vsphere-vm.tf
@@ -141,30 +141,3 @@ resource "vsphere_virtual_machine" "testvm" {
     ]
   }
 }
-
-#===============================================================
-# Docker Provider
-#===============================================================
-
-provider "docker" {
-  host = "tcp://${var.vm_ip}:2375/"
-}
-
-#===============================================================================
-# Docker Resources
-#===============================================================================
-
-resource "docker_image" "nginx" {
-  name          = "${data.docker_registry_image.nginx.name}"
-  pull_triggers = ["${data.docker_registry_image.nginx.sha256_digest}"]
-  depends_on = ["vsphere_virtual_machine.testvm"]
-}
-
-resource "docker_container" "nginx" {
-  name= "nginx"
-  image = "${docker_image.nginx.name}"
-  ports {
-    internal = "80"
-    external = "80"
-  }
-}
